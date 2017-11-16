b634c229a9c44b81b3d7106997a91f7fc5b0a8dc
Martin Zlámal <mrtnzlml@gmail.com>
2017-11-16 17:36:46 -0600

    Publish T9 exercise

---
+++ emptyObject.js
@@ -0,0 +1,7 @@
+/**
+ * This file is used ONLY to fix following Flow shit:
+ *
+ * [options]
+ * module.name_mapper='^apollo-server-micro$' -> '<PROJECT_ROOT>/emptyObject.js'
+ */
+module.exports = {};
